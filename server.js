7d19ff4128c96c8c67f01537c921606657e67ec2
Rudi Kelly <rudikellyy@gmail.com>
2018-10-26 15:49:11 +0100

    remove unneccessary comments

--- server.js
+++ server.js
@@ -2,11 +2,9 @@
 
 const express = require('express');
 
-// Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
