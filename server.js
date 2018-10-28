6a454c50cba3b929fdafbb826fd567d75aa75941
Rudi Kelly <rudikellyy@gmail.com>
2018-10-28 15:30:28 +0000

    Configure simple upload page

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const execFile = require('child_process').execFile;
+const multer = require('multer');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -14,6 +15,26 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('static'));
 
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({
+  storage: storage,
+  fileFilter: function(req, file, cb) {
+    var extCheck = file.originalname.split(".").slice(-1)[0] == "deb";
+    var mimeCheck = file.mimetype == "application/octet-stream";
+    if (extCheck && mimeCheck) {
+      return cb(null, true);
+    }
+    cb(null, false);
+  }
+});
+
 function refreshPackages() {
   var filePath = path.join(process.cwd(), "scan.py")
   execFile(filePath, ["--dir", "static/debs", "-o", "static"], function(err){
@@ -23,6 +44,18 @@ function refreshPackages() {
 
 app.get('/', (req, res) => {
   res.render('home', {title: "Home"});
+});
+
+app.get('/upload', (req, res) => {
+  res.render('upload', {title: "Upload"});
+});
+
+app.post('/upload', upload.single('deb'), (req, res) => {
+  if (req.file == null) {
+    res.send("invalid file");
+  } else {
+    res.send("received: " + req.file.originalname);
+  }
 });
 
 app.listen(PORT, HOST);
