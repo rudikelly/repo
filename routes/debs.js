77991daf6fcff199614ac3479f5015f6ca870ad2
Rudi Kelly <rudikellyy@gmail.com>
2018-11-05 17:00:02 +0000

    Grab data from request when downloading a .deb

--- routes/debs.js
+++ routes/debs.js
@@ -7,12 +7,18 @@ router.get('/deb/:deb', (req, res) => {
   res.sendFile(deb, {root: './deb'}, (err) => {
     if (err) {
       if (err.code == 'ENOENT') {
-        res.sendStatus(404);
+        return res.sendStatus(404);
       } else {
         throw err;
       }
     }
   });
+  const reqData = {
+    machine: req.headers['x-machine'],
+    firmware: req.headers['x-firmware'],
+    uuid: req.headers['x-unique-id'],
+    ua: req.headers['user-agent']
+  };
 });
 
 module.exports = router;
