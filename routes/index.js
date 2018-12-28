626d164d4b239fd135eb6fb6ec87992f4b62d886
Rudi Kelly <rudikellyy@gmail.com>
2018-12-28 16:11:08 +0000

    Fix index page rendering

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,24 @@
 
 const express = require('express');
 
+const User = require('../models/user');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('home');
+  if (req.session.userId) {
+    User.findOne({_id: req.session.userId}, (err, user) => {
+      if (err) throw err;
+      res.render('home',  {
+        user: {
+          firstName: user.firstName,
+        },
+      });
+    });
+  }
+  else {
+    res.render('home');
+  }
 });
 
 module.exports = router;
